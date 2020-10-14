dcf1350aab58c2a39d05d24e93a711e27d55f805
RafiAlfa17 <72843866+RafiAlfa17@users.noreply.github.com>
2020-10-14 18:59:16 +0700

    Add files via upload

---
+++ Fundamen_Sains_Data (Statistics and Probability)/Binomial Random Variables.R
@@ -0,0 +1,12 @@
+###Binomial Random Variable###
+
+
+###
+### For example, You roll a fair die 50 times
+### X is the number of times you get a six/6
+###and the probability of success is 1/6 in each of the 50 trials.
+### What is the probability
+### that we get X = 6 success out of 50 trials?
+
+dbinom(x = 6, size = 50, prob = 1/6)
+
